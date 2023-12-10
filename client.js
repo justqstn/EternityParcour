7393c4236f9b6d506d2a8fddb5fa4e1212e3e9ed
justqstn <128418414+justqstn@users.noreply.github.com>
2023-12-10 19:20:54 +0300

    Update client.js

--- client.js
+++ client.js
@@ -1,4 +1,6 @@
-// Бесконечный паркур
+try {
+    // Бесконечный паркур 
+// by just_qstn
 
 
 
@@ -11,43 +13,39 @@ Object.prototype.to_array = function()
     return [this.x, this.y, this.z]; 
 }
 
-// Прибавление какого-то числа к компонентам вектора
-Object.prototype.plus = function(num)
+// Превращает массив (вектор3) в объект
+Array.prototype.to_object = function()
 {
-    this.x += num; 
-    this.y += num; 
-    this.z += num; 
+    return {x: this[0], y: this[1], z: this[2]};
+}
+
+// Операция сложения
+Object.prototype.plus = function(vec)
+{
+    this.x += vec.x; 
+    this.y += vec.y; 
+    this.z += vec.z; 
 }
 
 // Константы
-const MAIN_TIMER_INTERVAL = 5; // Интервал генерации паркура
+const MAIN_TIMER_INTERVAL = 5, // Интервал генерации паркура
+    STRUCT_PER_SECOND = 5;     // Количество генерируемых структур в секунду
 
 // Константы - структуры
-const structure = {
-    blocks: {       // Блоки
-        pos: [],    // Позиции (трехмерные вектора)
-        id: [],     // Айди блока
-    },
-
-    new: function(pos, id) // Новая структура
-    {
-        let result = {};
-        Object.assign(result, this);
-        result.pos = pos;
-        result.id = id;
-        return result;
-    }
+// Структура
+const structure = function (pos, id) {    
+    this.pos = pos;   // Позиции (трехмерные вектора). Массив
+    this.id = id;     // Айди блока.
 }
 
 const structures = [];
 
 // Функции и объекты
 // Трехмерный вектор - точка в пространстве 
-const Vector3 = {
-    x: 0,
-    y: 0,
-    z: 0,
-    new: function(x, y, z) {return {x: x, y: y, z: z}}
+const Vector3 = function(x, y, z) {
+    this.x = x || 0;
+    this.y = y || 0;
+    this.z = z || 0;
 }
 
 // Перевод цвета из HEX строки в цвет в игре
@@ -65,7 +63,7 @@ const generate_struct = function(struct)
 {
     struct.blocks.pos.forEach(function(elem, index)
     {
-        MapEditor.SetBlock(elem, struct.blocks.id[index]);
+        MapEditor.SetBlock(last_pos.Value.to_object().plus(elem), struct.blocks.id[index]); 
     });
 }
 
@@ -74,14 +72,60 @@ let last_pos = Properties.GetContext().Get("last_pos"), // Позиция, с к
     main_timer = Timers.GetContext().Get("main");       // Основной таймер генерации паркура
  
 // Настройки
-last_pos.Value = Vector3.to_array(); // Задаем стартовую позицию
+e = AreaService.Get("start").Ranges.GetEnumerator();
+e.moveNext();
+last_pos.Value = e.Current.to_array(); // Задаем стартовую позицию
+delete e;
 
 // Создание команд
 Teams.Add("players", "<i><B><size=38>И</size><size=30>гроки</size></B>\nEternal Parcour by just_qstn</i>", hex_color("#9370DB"));
 let team = Teams.Get("players");
 
 // Интерфейс и лидерборд
+LeaderBoard.PlayerLeaderBoardValues = [
+	{
+		Value: "points",
+		DisplayName: "<B>о</B>чки",
+		ShortDisplayName: "<B>о</B>чки"
+	},
+	{
+		Value: "record",
+		DisplayName: "<B>р</B>екорд",
+		ShortDisplayName: "<B>р</B>екорд"
+	}
+];
+
+Ui.GetContext().TeamProp1.Value = {
+	Team: "players", Prop: "hint_left"
+};
+Ui.GetContext().TeamProp2.Value = {
+	Team: "players", Prop: "hint_right"
+};
+
+team.Properties.Get("hint_left").Value = "Бета-версия";
+team.Properties.Get("hint_right").Value = "by just_qstn";
 
 // События
+Teams.OnRequestJoinTeam.Add(function(p, t) {
+    t.Add(p);
+});
+
+Teams.OnPlayerChangeTeam.Add(function(p) {
+    p.Spawns.Spawn();
+});
 
 // Таймеры
+main_timer.OnTimer.Add(function() {
+    if (STRUCT_PER_SECOND > 1)
+    {
+        for (let i = 0; i < STRUCT_PER_SECOND; i++)
+        {
+            structures[Math.floor(Math.random() * (structures.length - 1))].generate_struct();
+        }
+    }
+    else
+    {
+        structures[Math.floor(Math.random() * (structures.length - 1))].generate_struct();
+    }
+});
+} catch(e) { Validate.ReportInvalid(e.name + " " + e.message);}
